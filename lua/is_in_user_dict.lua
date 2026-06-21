2c03c624dee38c11afdec4726ab591f586b6f66b
矶良 <441933726@qq.com>
2026-06-21 23:12:24 +0800

    Add Frost flypy schema

--- lua/is_in_user_dict.lua
+++ lua/is_in_user_dict.lua
@@ -14,7 +14,7 @@ function M.func(input, env)
     for cand in input:iter() do
         -- 用户词库，加上*号
         if cand.type == "user_phrase" then
-            cand.comment = cand.comment .. '*'
+            cand.comment = '*'
         end
         -- 用户置顶词
         -- if cand.type == "user_table" then
@@ -23,7 +23,7 @@ function M.func(input, env)
 
         -- 整句联想，加上𑄗符号
         if cand.type == 'sentence' then
-            cand.comment = cand.comment .. '∞'
+            cand.comment = '∞'
         end
         yield(cand)
     end
